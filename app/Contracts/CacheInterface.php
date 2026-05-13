efa0132fcdd5606c8696ce835ffd3a7c62b940d0
profosorm-source <profosorm@gmail.com>
2026-05-13 18:30:37 +0300

    Add files via upload

--- app/Contracts/CacheInterface.php
+++ app/Contracts/CacheInterface.php
@@ -8,4 +8,10 @@ interface CacheInterface
     public function delete(string $key): bool;
     public function increment(string $key, int $step = 1): int;
     public function getOrSet(string $key, callable $callback, ?int $ttl = null);
+    public function has(string $key): bool;
+    public function ttl(string $key): int;
+    public function flush(): bool;
+    public function redis(): ?\Redis;
+    public function driver(): string;
+    public function tags(array $tags);
 }
--- app/Contracts/CurrencyServiceInterface.php
+++ app/Contracts/CurrencyServiceInterface.php
@@ -10,6 +10,6 @@ interface CurrencyServiceInterface
     public function isIRT(): bool;
     public function isUSDT(): bool;
     public function getCurrencySymbol(): string;
-    public function formatAmount(float $amount): string;
+    public function formatAmount(float $amount, ?string $currency = null): string;
     public function getSectionCurrency(): string;
 }
