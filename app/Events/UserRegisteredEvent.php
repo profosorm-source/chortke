fdb6df314fea45120862967f1400a44004b51f20
profosorm-source <profosorm@gmail.com>
2026-05-16 14:30:50 +0300

    Add files via upload

--- app/Events/UserRegisteredEvent.php
+++ app/Events/UserRegisteredEvent.php
@@ -12,12 +12,14 @@ class UserRegisteredEvent extends Event
         public readonly int $userId,
         public readonly string $email,
         public readonly string $ipAddress,
+        public readonly ?string $plainToken = null,
         public readonly \DateTimeInterface $occurredAt = new \DateTimeImmutable()
     ) {
         parent::__construct([
             'user_id' => $userId,
             'email' => $email,
             'ip' => $ipAddress,
+            'plain_token' => $plainToken,
             'occurred_at' => $occurredAt->format(\DateTime::ATOM)
         ]);
     }
