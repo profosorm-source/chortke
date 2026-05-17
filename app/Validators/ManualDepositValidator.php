2ddf43b74f794e416bb80d822ad2004db9397e2b
profosorm-source <profosorm@gmail.com>
2026-05-18 02:17:53 +0300

    Add files via upload

--- app/Validators/ManualDepositValidator.php
+++ app/Validators/ManualDepositValidator.php
@@ -8,7 +8,7 @@ class ManualDepositValidator extends Validator
 {
     protected array $rules = [
         'bank_card_id' => 'required|numeric',
-        'amount' => 'required|string',
+        'amount' => 'required|numeric|min:10000|max:100000000|regex:/^\d+(\.\d{1,4})?$/',
         'tracking_code' => 'required|string|min:5|max:50',
         'deposit_date' => 'required|date',
         'deposit_time' => 'required|string',
@@ -18,7 +18,10 @@ class ManualDepositValidator extends Validator
     protected array $messages = [
         'bank_card_id.required' => 'انتخاب کارت بانکی الزامی است',
         'amount.required' => 'مبلغ الزامی است',
+        'amount.numeric' => 'مبلغ باید عددی باشد',
         'amount.min' => 'حداقل مبلغ ۱۰,۰۰۰ تومان است',
+        'amount.max' => 'حداکثر مبلغ ۱۰۰,۰۰۰,۰۰۰ تومان است',
+        'amount.regex' => 'فرمت مبلغ نامعتبر است (حداکثر ۴ رقم اعشار مجاز است)',
         'tracking_code.required' => 'شماره پیگیری الزامی است',
         'deposit_date.required' => 'تاریخ الزامی است',
         'deposit_time.required' => 'ساعت الزامی است'
