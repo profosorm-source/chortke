fa3d1b0e9bbadddf2f5f5c0b193cf4ce77830788
profosorm-source <profosorm@gmail.com>
2026-05-19 22:45:29 +0300

    Add files via upload

--- app/Controllers/ContactController.php
+++ app/Controllers/ContactController.php
@@ -21,12 +21,22 @@ class ContactController extends BaseController
      */
     public function send()
     {
-        $result = $this->contactService->sendMessage($this->request->all());
+        $data = [
+            'name' => $this->request->input('name'),
+            'email' => $this->request->input('email'),
+            'subject' => $this->request->input('subject'),
+            'message' => $this->request->input('message'),
+            'website' => $this->request->input('website'),
+            'captcha_token' => $this->request->input('captcha_token'),
+            'captcha_response' => $this->request->input('captcha_response'),
+        ];
+
+        $result = $this->contactService->sendMessage($data);
 
         if (!$result['success']) {
-            return $this->response->error($result['message'], $result['errors'] ?? [], $result['status_code'] ?? 400);
+            return $this->response->json(['success' => false, 'message' => $result['message'], 'errors' => $result['errors'] ?? []], $result['status_code'] ?? 422);
         }
 
-        return $this->response->success($result['message']);
+        return $this->response->json(['success' => true, 'message' => $result['message']]);
     }
 }
